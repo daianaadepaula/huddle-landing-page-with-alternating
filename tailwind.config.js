e4bada9cef884230900b1e10760dd248760797e8
daianaadepaula <daianaadepaula1@gmail.com>
2023-07-12 10:46:40 -0300

    feat: add header

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+			colors: {
+				'pink-primary': 'hsl(322, 100%, 66%)',
+				'very-pale-cyan': 'hsl(193, 100%, 96%)',
+				'very-dark-cyan': 'hsl(192, 100%, 9%)',
+				'grayish-blue': 'hsl(208, 11%, 55%)',
+			},
+			fontFamily: {
+				open: ['Open Sans', 'sans-serif'],
+				poppins: ['Poppins', 'serif'],
+			},
+			backgroundImage: {
+        'hero-desktop': "url('../images/bg-hero-desktop.svg')",
+        'hero-mobile': "url('../images/bg-hero-mobile.svg')",
+      },
+		},
+  },
+  plugins: [],
+}
+
